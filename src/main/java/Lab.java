41179b1a32f8374ffc22045cc938855d54686115
Emmanuel Okobi <ebokobi@aggies.ncat.edu>
2023-02-22 23:28:58 +0000

    Completed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,11 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
+        int[] nums = new int[3];
+
+        for(int i = 0; i < nums.length; i++) {
+            System.out.println(nums[3]);
+        }
 
     }
 }
